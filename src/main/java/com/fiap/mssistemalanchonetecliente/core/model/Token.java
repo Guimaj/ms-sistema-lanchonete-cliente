2204dd8024425b7b9beb664590f9233b97fa37b5
= <=>
2024-12-01 17:50:59 -0300

    aumentando cobertura

--- src/main/java/com/fiap/mssistemalanchonetecliente/core/model/Token.java
+++ src/main/java/com/fiap/mssistemalanchonetecliente/core/model/Token.java
@@ -2,17 +2,11 @@ package com.fiap.mssistemalanchonetecliente.core.model;
 
 import lombok.*;
 
-
 @AllArgsConstructor
-@NoArgsConstructor
 public class Token {
     private String jwt;
 
     public String getJwt() {
         return jwt;
-    }
-
-    public void setJwt(String jwt) {
-        this.jwt = jwt;
     }
 }
